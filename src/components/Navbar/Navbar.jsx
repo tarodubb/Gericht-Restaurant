0864c82400b18fafd90ba87a3b31a0dfb4354ecb
maxwell wong <midorikawamaxwell@gmail.com>
2023-03-23 23:16:29 +0900

    navbar components imports

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { GithHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
+import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
