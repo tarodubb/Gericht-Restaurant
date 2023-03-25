969f2e381dd49566f953f37e07dc6284946dc79a
maxwell wong <midorikawamaxwell@gmail.com>
2023-03-25 19:44:50 +0900

    styling for wine & beer, cocktail menu section

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -3,21 +3,19 @@ import React from 'react';
 import './MenuItem.css';
 
 const MenuItem = ({ title, price, tags }) => (
-  <div className='app__menuitem'>
+  <div className="app__menuitem">
     <div className="app__menuitem-head">
-      {/* menu item title */}
       <div className="app__menuitem-name">
-        <p className="p__cormorant" style={ {color: '#DCCA87'} }>{title}</p>
+        <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
       </div>
-      <div className='app__menuitem-dash'/>
-      {/* menu item price */}
-      <div className="app__menuitem-name">
+      <div className="app__menuitem-dash" />
+      <div className="app__menuitem-price">
         <p className="p__cormorant">{price}</p>
       </div>
     </div>
 
     <div className="app__menuitem-sub">
-      <p className="p__opensans" style={{ color: '#AAA'}}>{tags}</p>
+      <p className="p__opensans" style={{ color: '#AAAAAA' }}>{tags}</p>
     </div>
   </div>
 );
