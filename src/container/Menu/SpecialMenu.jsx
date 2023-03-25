b6efd79faaff16d55785138f1fb337da3e7e3e25
maxwell wong <midorikawamaxwell@gmail.com>
2023-03-25 19:11:36 +0900

    created menu-item component and styling

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -12,29 +12,32 @@ const SpecialMenu = () => (
     </div>
 
     <div className="app__specialMenu-menu">
-
+      {/* List of wines */}
       <div className="app__specialMenu-menu_wine flex__center">
         <p className='app__specialMenu_menu_heading'>Wine and Beer</p>
         <div className="app__specialMenu_menu_items">
           {data.wines.map((wine, index) => (
-            <p>{wine.title}</p>
+            <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags}/>
           ))}
         </div>
       </div>
-
-
+      {/* Special Menu img */}
       <div className="app__specialMenu-menu__img">
         <img src={images.menu} alt="menu img" />
       </div>
-
+      {/* List of cocktails */}
       <div className="app__specialMenu-menu_cocktails flex__center">
         <p className='app__specialMenu_menu_heading'>Wine and Beer</p>
         <div className="app__specialMenu_menu_items">
           {data.cocktails.map((cocktail, index) => (
-            <p>{cocktail.title}</p>
+            <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags}/>
           ))}
         </div>
       </div>
+    </div>
+
+    <div style={{marginTop: '15px'}}>
+      <button type='button' className='custom__button'>View More</button>
     </div>
   </div>
 );
