fc1037682d51941cf3f3b4ce3bc7cfc4f780405f
maxwell wong <midorikawamaxwell@gmail.com>
2023-03-27 01:15:16 +0900

    made footer section layout responsive for mobile devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <li className='p__opensans'><a href="#about">About</a></li>
         <li className='p__opensans'><a href="#menu">Menu</a></li>
         <li className='p__opensans'><a href="#awards">Awards</a></li>
-        <li className='p__opensans'><a href="#contacts">Contacts</a></li>
+        <li className='p__opensans'><a href="#contact">Contact</a></li>
       </ul>
       <div className="app__navbar-login">
         <a href="#login" className='p__opensans'>Log In / Register</a>
