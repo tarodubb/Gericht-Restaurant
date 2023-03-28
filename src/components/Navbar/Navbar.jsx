44b009673f77e3680d4f6fbdd755f6e61bbe57e7
maxwell wong <midorikawamaxwell@gmail.com>
2023-03-28 21:47:30 +0900

    made the hamburger menu items close the overlay menu when an item is clicked on

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,9 +21,9 @@ const Navbar = () => {
         <li className='p__opensans'><a href="#contact">Contact</a></li>
       </ul>
       <div className="app__navbar-login">
-        <a href="#login" className='p__opensans'>Log In / Register</a>
+        <a href="#" className='p__opensans'>Log In / Register</a>
         <div />
-        <a href="/" className='p__opensans'>Book Table</a>
+        <a href="#" className='p__opensans'>Book Table</a>
       </div>
       <div className="app__navbar-smallscreen">
         <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)}/>
@@ -32,11 +32,11 @@ const Navbar = () => {
           <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
             <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)}/>
             <ul className='app__navbar-smallscreen_links'>
-              <li className='p__opensans'><a href="#home">Home</a></li>
-              <li className='p__opensans'><a href="#about">About</a></li>
-              <li className='p__opensans'><a href="#menu">Menu</a></li>
-              <li className='p__opensans'><a href="#awards">Awards</a></li>
-              <li className='p__opensans'><a href="#contacts">Contacts</a></li>
+              <li className='p__opensans' onClick={() => setToggleMenu(false)}><a href="#home">Home</a></li>
+              <li className='p__opensans' onClick={() => setToggleMenu(false)}><a href="#about">About</a></li>
+              <li className='p__opensans' onClick={() => setToggleMenu(false)}><a href="#menu">Menu</a></li>
+              <li className='p__opensans' onClick={() => setToggleMenu(false)}><a href="#awards">Awards</a></li>
+              <li className='p__opensans' onClick={() => setToggleMenu(false)}><a href="#contacts">Contacts</a></li>
             </ul>
           </div>
         )}
